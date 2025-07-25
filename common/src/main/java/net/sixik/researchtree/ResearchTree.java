5033cd4c1229513a79adba3dfa9a32008dab2da6
SagaDeoMissTeam <142807122+SagaDeoMissTeam@users.noreply.github.com>
2025-07-26 02:00:05 +0300

    Add KeyBinding

--- common/src/main/java/net/sixik/researchtree/ResearchTree.java
+++ common/src/main/java/net/sixik/researchtree/ResearchTree.java
@@ -5,6 +5,8 @@ import dev.architectury.event.events.client.ClientTickEvent;
 import dev.architectury.platform.Platform;
 import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import dev.ftb.mods.ftblibrary.ui.Theme;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.KeyMapping;
 import net.sixik.researchtree.config.ModConfig;
 import net.sixik.researchtree.network.ResearchTreeNetwork;
@@ -15,8 +17,9 @@ import org.lwjgl.glfw.GLFW;
 public final class ResearchTree {
     public static final String MODID = "researchtree";
 
-    public static final String SDMSHOP_CATEGORY = "key.category.sdmshopr";
-    public static final String KEY_NAME = "key.sdm.debug";
+    public static final String SDMSHOP_CATEGORY = "key.category.researchtree";
+    public static final String KEY_NAME = "key.researchtree.open";
+    @Environment(EnvType.CLIENT)
     public static KeyMapping KEY_SHOP = new KeyMapping(KEY_NAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_O, SDMSHOP_CATEGORY);
 
     public static final ModConfig MOD_CONFIG =
@@ -24,9 +27,11 @@ public final class ResearchTree {
 
     public static void init() {
         if(Platform.isDevelopmentEnvironment()) {
-            developer();
             Theme.renderDebugBoxes = false;
         }
+
+        if(Platform.getEnv() == EnvType.CLIENT)
+            registerKeyBinding();
 
         ResearchTreeModEvents.init();
         ModRegisters.init();
@@ -34,7 +39,8 @@ public final class ResearchTree {
         MOD_CONFIG.load();
     }
 
-    public static void developer() {
+    @Environment(EnvType.CLIENT)
+    public static void registerKeyBinding() {
         KeyMappingRegistry.register(KEY_SHOP);
 
         ClientTickEvent.CLIENT_PRE.register((instance -> {
